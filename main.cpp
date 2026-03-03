ea0b55808403f34474cf045106bec37f4c6a1d8a
Eazy <111327362+Eazythedev@users.noreply.github.com>
2026-03-03 07:31:46 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -26,27 +26,49 @@ int main()
 
     _getch();
 
+    cout << "____________________________________________________________________________________________" << "\n";
     cout << "The story begins with you." << "\n";
+    cout << "____________________________________________________________________________________________" << "\n";
     cout << "Press the any key to coninue" << "\n";
 
     _getch();
 
+    cout << "____________________________________________________________________________________________" << "\n";
     cout << "Grass dances in the wind. Colorful leaves rustle and dance over the fields." << "\n";
+    cout << "____________________________________________________________________________________________" << "\n";
     cout << "Press the any key to coninue" << "\n";
 
     _getch();
 
+    cout << "____________________________________________________________________________________________" << "\n";
     cout << "Though the sky has clouds, the light shines over the land." << "\n";
+    cout << "____________________________________________________________________________________________" << "\n";
     cout << "Press the any key to coninue" << "\n";
 
+    _getch();
+
+    cout << "____________________________________________________________________________________________" << "\n";
     cout << "You begin by going out to gather wood for a nearby village." << "\n";
+    cout << "____________________________________________________________________________________________" << "\n";
     cout << "Press the any key to coninue" << "\n";
 
-    // cout << "";
+    _getch();
+
+    cout << "____________________________________________________________________________________________" << "\n";
+    cout << "While out in the nearby forest, you find a sword stuck in a tree. You take the sword." << "\n";
+    cout << "____________________________________________________________________________________________" << "\n";
+    cout << "Press the any key to coninue" << "\n";
+  
+    _getch();
 
     // Create decision logic for first decision logic for first player decision in the game.
-    
-    _getch();
+
+    cout << "____________________________________________________________________________________________" << "\n";
+    cout << "You exit out of the forest to find an abandoned hut.";
+    cout << "____________________________________________________________________________________________" << "\n";
+
+
+    // _getch();
 
     // Next thing.
     
